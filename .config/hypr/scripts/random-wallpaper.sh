5e34bb5bd400a879d0cb68e24782300b4dc883fe
highlyreactiveNeon <97377851+highlyreactiveNeon@users.noreply.github.com>
2023-10-23 09:11:02 +0530

    feat: same wallpaper will not be chosen in a row

--- .config/hypr/scripts/random-wallpaper.sh
+++ .config/hypr/scripts/random-wallpaper.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-swww img `find $WALLPAPER_PATH -type f | shuf -n 1` --transition-type any --transition-fps 144
+get_wallpaper_path() {
+    local path=$(swww query | awk -F'image: ' '{print $2}')
+    echo "$path"
+}
+
+while true; do
+    random_path=$(find $WALLPAPER_PATH -type f | shuf -n 1)
+
+    if [[ "$random_path" != "$(get_wallpaper_path)" ]]; then
+        break
+    fi    
+done
+
+swww img "$random_path" --transition-type any --transition-fps 144
