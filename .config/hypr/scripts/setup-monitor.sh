b8f0069ca243f24e282ee714c482b56e1f040673
highlyreactiveNeon <97377851+highlyreactiveNeon@users.noreply.github.com>
2023-12-31 19:30:58 +0530

    chore

--- .config/hypr/scripts/setup-monitor.sh
+++ .config/hypr/scripts/setup-monitor.sh
@@ -5,6 +5,6 @@ set -e
 file_path=$HOME/.config/hypr/db/refresh-rate
 refresh_rate=$(< "$file_path")
 
-echo $refresh_rate
+echo "$refresh_rate"
 
 hyprctl keyword monitor ,1920x1080@$refresh_rate,auto,1
