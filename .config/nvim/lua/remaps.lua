fa0c487416fb1929c431c12f890f6902cb14600f
highlyreactiveNeon <97377851+highlyreactiveNeon@users.noreply.github.com>
2024-03-02 00:24:52 +0530

    update config

--- .config/nvim/lua/remaps.lua
+++ .config/nvim/lua/remaps.lua
@@ -1,3 +1,22 @@
+--- uncomment the below line if you are using netrw
+-- vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
 -- replacing this to enable diagnostic on "InsertLeave" event. Me comfotable with <C-c> :)
 vim.keymap.set("i", "<C-c>", "<C-[>")
 vim.keymap.set("i", "<C-[>", "<C-c>")
+
+-- centering the cursor while half-page jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- for copying into system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- like alt+arrow in VSCode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- TODO: add delete to void register
+-- TODO: for managing buffers
+-- TODO: add Primeagean's remaps
